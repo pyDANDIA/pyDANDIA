52717104d78d35e2aeea01550616a06dc0b18c4e
rachel3834 <rstreet@lcogt.net>
2017-07-15 22:22:51 -0700

    Implemented code to read old-format reduction config data and convert it to new-format metadata file

--- trials/metadata/convert_red_metadata.py
+++ trials/metadata/convert_red_metadata.py
@@ -5,7 +5,9 @@ Created on Thu Jul 13 17:59:16 2017
 @author: rstreet
 """
 from os import path
-from sys import exit
+from sys import exit, argv
+import xml.sax
+import metadata
 
 def convert_red_metadata():
     """Function to convert the configuration parameter files and trendlogs
@@ -14,9 +16,26 @@ def convert_red_metadata():
     
     config = get_config()
 
-    event_info = read_event_info(config)
+    meta = metadata.MetaData()
     
+    event_info = read_event_info(config)
+    meta.set_pars(event_info)
+    
+    meta.set_reduction_paths(config['red_dir'])
+    
+    red_config = read_red_config(config)
+    for key, value in red_config.items():
+        meta.set_pars(red_config)
+    
+    meta.inventory = read_data_inventory(config)
+    
+    trends = ['imred', 'gimred', 'dimred']
+    for t in trends:
+        (header, data) = read_trendlog(config[t])
+        setattr(meta,t,[ header, data ])
 
+    meta.write()
+        
 def get_config():
     """Function to harvest user-specified configuration parameters"""
     
@@ -29,7 +48,7 @@ def get_config():
     
     config['event_code'] = path.basename(config['red_dir'])
     config['filter'] = config['event_code'].split('_')[-1]
-    config['event_file'] = path.join(config['red_dir'],\
+    config['event_info'] = path.join(config['red_dir'],\
                                 config['event_code']+'.Event.Info')
     config['red_config'] = path.join(config['red_dir'],\
                                 config['event_code']+'.Red.Config')
@@ -47,13 +66,13 @@ def get_config():
 def read_event_info(config):
     """Function to read the parameters from an old-format Event.Info file"""
 
-    keys = {'Object:':'object' ,
+    keys = {'Object:':'field' ,
             'Year:': 'year',
             'Network:': 'network',
             'Site:': 'site',
             'Enclosure:': 'enclosure',
-            'Telescope:':'tel',
-            'Instrument:':'instrum',
+            'Telescope:':'telescope',
+            'Instrument:':'instrument',
             'Filter 1:':'f1',
             'Filter 2:':'f2',
             'Filter 3:':'f3',
@@ -71,10 +90,115 @@ def read_event_info(config):
         for line in file_lines:
             if line.__contains__(header) == True:
                 event_info[keys[header]] = line.split()[-1]
-                
-    return event_info
-      
     
+    event_info['filter'] = dominant_filter(event_info)
+    
+    return event_info
+
+def dominant_filter(event_info):
+    """Function to decide which of the parsed filters is the dominant one."""
+    
+    flist = [str(event_info['f1']).lower(), 
+                        str(event_info['f2']).lower(), 
+                        str(event_info['f3']).lower()]
+    filters = []
+    for f in flist:
+        if f != 'air' and f != 'none' and f != 'clean':
+            filters.append(f)
+    
+    if len(filters) == 1:
+        filter_name = filters[0]
+    
+    else:
+        filter_name = '/'.join(filters)
+        
+    return filter_name
+ 
+class ConfigHandler(xml.sax.handler.ContentHandler):
+    """Parse config file parameters"""
+    
+    def __init__(self):
+        self.iValue = 0
+        self.mapping = {}
+    
+    def startElement(self,name,attributes):
+        if name == 'parameter':
+            self.buffer = ""
+            self.paramName = attributes["name"]
+        elif name == 'value':
+            self.iValue = 1
+    
+    def characters(self, data):
+        if self.iValue == 1:
+            self.buffer += data
+    
+    def endElement(self, name):
+        if name == 'value':
+            self.iValue = 0
+            self.mapping[self.paramName] = self.buffer
+ 
+
+def read_red_config(config):
+    """Function to read the parameters from an old-format Red.Config file"""
+    
+    red_config = {}
+    if path.isfile(config['red_config']) == False:
+        print('Error: Cannot find reduction configuration at '+config['red_config'])
+
+    parser = xml.sax.make_parser(  )
+    red_config = ConfigHandler( )
+    parser.setContentHandler(red_config)
+    parser.parse(config['red_config'])
+    
+    return red_config.mapping
+    
+def read_data_inventory(config):
+    """Function to read the data inventory from an old-format Inventory file.
+    Receives the full path to the file and returns a dictionary of the entries 
+    of the form:
+    Images[imname] = [eventname, eventname, imdate, imtime, procstatus, nproc]
+    The framestatus parameter indicates what processing status of frame to 
+    search for, ie whether to list (P)rocessed or (U)nprocessed frames.
+    """
+    
+    inventory = []
+    
+    if path.isfile(config['inventory']) == False:
+        print('Error: Cannot find the data inventory at '+config['inventory'])
+        exit()
+    
+    file_lines = open(config['inventory'],'r').readlines()
+    
+    for line in file_lines:
+        entries = line.split()
+        inventory.append(entries)
+    
+    return inventory
+
+def read_trendlog(trend_file):
+    """Function to read a generic-format trendlog from DanDIA in the old format"""
+    
+    header = ''
+    data = {}
+
+    if path.isfile(trend_file) == False:
+        print('Error: Cannot find trendlog file '+trend_file)
+        exit()
+    
+    file_lines = open(trend_file,'r').readlines()
+
+    for line in file_lines:
+        if len(line) > 0:
+            if line[0:1] == '#':
+                header = line.replace('\n','').split()
+            else:
+                entries = line.replace('\n','').split()
+                tlist = []
+                for item in entries[1:]:
+                    tlist.append(float(item))
+                data[path.basename(entries[0])] = tlist
+
+    return header, data
 
 
 if __name__ == '__main__':
