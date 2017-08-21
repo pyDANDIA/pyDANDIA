7361a5f05a0f27b4bb7f1668209e29d2470c11d6
ytsapras <huluvu38@gmail.com>
2017-08-21 09:57:44 +0200

    stage1 update

--- pyDANDIA/stage1.py
+++ pyDANDIA/stage1.py
@@ -27,7 +27,7 @@ import glob
 sys.path.append('../pyDANDIA/')
 import metadata
 import stage0
-reduction_metadata = stage0.create_or_load_the_reduction_metadata('./', metadata_name='pyDANDIA_metadata.fits', verbose=True)
+reduction_metadata = stage0.create_or_load_the_reduction_metadata('.', metadata_name='test_metadata.fits', verbose=True)
 
 path_to_images = '../trials/data/'
 images = glob.glob(path_to_images+'*fits')
@@ -37,14 +37,26 @@ conf_dict = config.read_config('../Config/config.json')
 gain =  conf_dict['gain']['value']
 read_noise = conf_dict['ron']['value']
 
+# Set up holiding arrays
+names_arr = []
+sky_arr = []
+fwhm_x_arr = []
+fwhm_y_arr = []
+corr_xy_arr = []
+
 # For the set of given images set the metadata information
 for im in images:
     sky, fwhm_y, fwhm_x, corr_xy = starfind.starfind(im, plot_it=False, write_log=False)
-    # Write to the metadata file
-    reduction_metadata.create_data_inventory_layer([im])
-    reduction_metadata.save_a_layer_to_file(metadata_directory='../trials/',metadata_name='pyDANDIA_metadata.fits',key_layer='data_inventory' )
-    reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='fwhm_x', new_column_data=[fwhm_x], new_column_format='float', new_column_unit='arcsec' )
-    reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='fwhm_y', new_column_data=[fwhm_y], new_column_format='float', new_column_unit='arcsec' )
-    reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='sky', new_column_data=[sky], new_column_format='float', new_column_unit='ADU counts' )
-    reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='corr_xy', new_column_data=[corr_xy], new_column_format='float', new_column_unit=None )
-    reduction_metadata.save_a_layer_to_file(metadata_directory='../trials/',metadata_name='pyDANDIA_metadata.fits',key_layer='data_inventory' )
+    names_arr.append(im.split('/')[-1])
+    sky_arr.append(sky)
+    fwhm_x_arr.append(fwhm_y)
+    fwhm_y_arr.append(fwhm_x)
+    corr_xy_arr.append(corr_xy)
+
+# Write to the metadata file
+reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='im_name', new_column_data=names_arr, new_column_format='str', new_column_unit=None )
+reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='fwhm_x', new_column_data=fwhm_x, new_column_format='float', new_column_unit='arcsec' )
+reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='fwhm_y', new_column_data=fwhm_y, new_column_format='float', new_column_unit='arcsec' )
+reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='sky', new_column_data=sky, new_column_format='float', new_column_unit='ADU counts' )
+reduction_metadata.add_column_to_layer(key_layer='data_inventory', new_column_name='corr_xy', new_column_data=corr_xy, new_column_format='float', new_column_unit=None )
+reduction_metadata.save_a_layer_to_file(metadata_directory='.',metadata_name='test_metadata.fits',key_layer='data_inventory' )
