4a534e675be1045a008325426626780fd708b3a2
rachel3834 <rstreet@lcogt.net>
2017-10-18 18:09:22 -0700

    First commit of code to model the image sky background, and placeholders for the PSF photometry code

---
+++ pyDANDIA/photometry.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 15:42:26 2017
+
+@author: rstreet
+"""
+
+from os import path
+from sys import exit
+import numpy as np
+import logs
+import metadata
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+def build_psf():
+    """Function to build the Point Spread Function (PSF) for an image"""
+    
+    # Subtract the background sky from the reference image
+    return 0
+    
