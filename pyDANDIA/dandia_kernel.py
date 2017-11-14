040a435e7bfa2ceb8c2e514df223975e083874e6
mpgh <markus.hundertmark@uni-heidelberg.de>
2017-11-14 16:10:59 +0100

    first cython version of the kernel

---
+++ pyDANDIA/dandia_kernel.py
@@ -0,0 +1,102 @@
+from __future__ import division
+import numpy as np
+from numpy.linalg import inv
+from astropy.io import fits
+from umatrix_routine import umatrix_construction
+import scipy.ndimage as ndimage
+
+
+def read_images():
+    data_image = fits.open('data_image.fits')
+    ref_image = fits.open('ref_image.fits')
+    return np.array(ref_image[0].data, float), np.array(data_image[1].data, float)
+
+# Depends on model_image
+
+
+def noise_model(model_image, gain, readout_noise,  flat=None, initialize=None):
+    if initialize == True:
+        model_image = ndimage.gaussian_filter(
+            np.copy(model_image), sigma=2, order=0)
+
+    variance = np.copy(model_image)
+    variance = variance / gain
+    if flat != None:
+        variance = variance / flat
+        ron_term = readout_noise**2 / flat**2
+    else:
+        ron_term = readout_noise**2 * np.ones(np.shape(model_image))
+    noise_image = variance + ron_term
+
+    return noise_image
+
+
+def u_matrix_unimproved(data_image, reference_image, ker_size, weights=None):
+    """
+    The naive kernel solution is supposed to implement
+    the Bramich 2008 implementation of the kernel solution as
+    outlined in the original paper
+
+    The data image is indexed using i,j
+    the reference image should have the same shape as the data image
+    the kernel is indexed via l,m and that is converted into 
+    single 
+
+    kernel_size requires as input the edge length of the kernel
+
+
+    it consists of k_lm and a background b0 
+    k_lm, where l and m correspond to the kernel pixel indices
+    The resulting vector b is obtained from the matrix U_l,m,l
+    In practice we are unrolling the index
+    we assume a quadratic kernel
+    """
+
+    if np.shape(data_image) != np.shape(reference_image):
+        return None
+
+    if ker_size:
+        if ker_size % 2 == 0:
+            kernel_size = ker_size + 1
+        else:
+            kernel_size = ker_size
+
+    if weights == None:
+        weights = noise_model(reference_image, 1., 0.,
+                              flat=None, initialize=True)
+        weights = 1. / weights
+
+    # Prepare/initialize indices, vectors and matrices
+    pandq = []
+    n_kernel = kernel_size * kernel_size
+    for lidx in range(0, kernel_size):
+        for midx in range(0, kernel_size):
+            pandq.append((lidx, midx))
+
+    ni_image, nj_image = np.shape(data_image)
+    u_matrix, b_vector = umatrix_construction(
+        reference_image, data_image, weights, pandq, n_kernel, kernel_size)
+
+    # last entry b_vector
+    b_vector[-1] = np.sum(data_image[1:-1][1:-1] * weights[1:-1][1:-1])
+
+    return u_matrix, b_vector
+
+
+if __name__ == '__main__':
+    ref_image, data_image = read_images()
+
+    kernel_size = 7
+    # construct U matrix
+    u_matrix, b_vector = u_matrix_unimproved(
+        data_image, ref_image, kernel_size, weights=None)
+
+    kernel_image = np.dot(inv(u_matrix), b_vector)[:-1]
+    kernel_image = np.roll(kernel_image[:-1], int(kernel_image.size / 2))
+    kernel_image = kernel_image.reshape((kernel_size, kernel_size))
+
+    hl = fits.PrimaryHDU(np.abs(u_matrix))
+    hl.writeto('tst_umatrix.fits', overwrite=True)
+
+    hl2 = fits.PrimaryHDU(np.abs(kernel_image))
+    hl2.writeto('kernel_image.fits', overwrite=True)
---
+++ pyDANDIA/umatrix_routine.pyx
@@ -0,0 +1,66 @@
+from __future__ import division
+import numpy as np
+cimport numpy as np
+cimport cython
+DTYPE = np.float64
+ctypedef np.float64_t DTYPE_t
+
+# compile suggestion: gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/somepath/include/python2.7 -o umatrix_routine.so umatrix_routine.c
+
+@cython.boundscheck(False) # turn off bounds-checking
+@cython.wraparound(False)  # turn off negative index wrapping
+
+
+def umatrix_construction(np.ndarray[DTYPE_t, ndim = 2] reference_image,np.ndarray[DTYPE_t, ndim = 2] data_image,np.ndarray[DTYPE_t, ndim = 2] weights, pandq, n_kernel_np, kernel_size_np):
+
+    cdef int ni_image = np.shape(data_image)[0]
+    cdef int nj_image = np.shape(data_image)[1]
+    cdef double sum_acc = 0.
+    cdef int idx_l,idx_m,idx_l_prime,idx_m_prime,idx_i,idx_j
+    cdef int kernel_size = np.int(kernel_size_np)
+    cdef int n_kernel = np.int(n_kernel_np)
+    cdef np.ndarray u_matrix = np.zeros([n_kernel + 1, n_kernel + 1], dtype=DTYPE)
+
+
+    for idx_p in range(n_kernel):
+        for idx_q in range(idx_p,n_kernel):
+            sum_acc = 0.
+            idx_l, idx_m = pandq[idx_p]
+            idx_l_prime, idx_m_prime = pandq[idx_q]
+            for idx_i in range(ni_image-kernel_size+1):
+                for idx_j in range(nj_image-kernel_size+1):         
+                    sum_acc += reference_image[idx_i + idx_l, idx_j + idx_m] * reference_image[idx_i + idx_l_prime,idx_j + idx_m_prime]  * weights[idx_i, idx_j]
+            u_matrix[idx_p, idx_q] = sum_acc
+            u_matrix[idx_q, idx_p] = sum_acc
+
+    for idx_p in [n_kernel-1]:
+        for idx_q in range(n_kernel):
+            sum_acc = 0.
+            idx_l = kernel_size
+            idx_m = kernel_size
+            idx_l_prime, idx_m_prime = pandq[idx_q]
+            for idx_i in range(ni_image-kernel_size+1):
+                for idx_j in range(nj_image-kernel_size+1):               
+                    sum_acc += reference_image[idx_i + idx_l_prime, idx_j + idx_m_prime] * weights[idx_i, idx_j]
+            u_matrix[idx_q, idx_p] = sum_acc
+
+    for idx_p in range(n_kernel):
+        for idx_q in [n_kernel]:
+            sum_acc = 0.
+            idx_l, idx_m = pandq[idx_p]
+            idx_l_prime = kernel_size
+            idl_m_prime = kernel_size 
+            for idx_i in range(ni_image-kernel_size+1):
+                for idx_j in range(nj_image-kernel_size+1):
+                    sum_acc += u_matrix[idx_p, idx_q] + reference_image[idx_i + idx_l, idx_j + idx_m] * weights[idx_i, idx_j]
+
+    cdef np.ndarray b_vector = np.zeros([n_kernel + 1], dtype=DTYPE)
+    for idx_p in range(n_kernel):
+        idx_l, idx_m = pandq[idx_p]
+        for idx_i in range(1,ni_image-kernel_size+1):
+           for idx_j in range(1,nj_image-kernel_size+1):
+                sum_acc += data_image[idx_i, idx_j] * reference_image[idx_i + idx_l, idx_j + idx_m] * weights[idx_i, idx_j]
+        b_vector[idx_p] = sum_acc
+
+    return u_matrix, b_vector
+
