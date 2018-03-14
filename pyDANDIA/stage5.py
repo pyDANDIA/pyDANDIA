f26df95a7f1d4a738e806ddef77c7e958b0b12a5
mpgh <markus.hundertmark@uni-heidelberg.de>
2018-03-14 16:10:39 +0100

    stage5 dev

--- pyDANDIA/stage5.py
+++ pyDANDIA/stage5.py
@@ -9,10 +9,11 @@
 #      astropy 1.0+
 #      scipy 1.0+
 ######################################################################
-import numpy as np
 import os
+import numpy as np
 from astropy.io import fits
 from scipy.signal import convolve2d
+from read_images_stage5 import open_reference,  open_images
 import sys
 
 import config_utils
@@ -35,7 +36,8 @@ def run_stage5(setup):
     log = logs.start_stage_log(setup.red_dir, 'stage5', version=stage5_version)
     log.info('Setup:\n' + setup.summary() + '\n')
     try:
-        from umatrix_routine import umatrix_construction
+        from umatrix_routine import umatrix_construction, umatrix_bvector_construction, bvector_construction
+         
     except ImportError:
         log.info('Uncompiled cython code, please run setup.py: e.g.\n python setup.py build_ext --inplace')
         status = 'KO'
@@ -54,7 +56,7 @@ def run_stage5(setup):
         if float(stats_entry['FWHM_Y'])> fwhm_max:
             fwhm_max = stats_entry['FWHM_Y']
     
-    kernel_size = 2. * fwhm_max #multiplicative factor to use s0 ker_rad...
+    kernel_size = reduction_metadata.reduction_parameters[1]['KERRAD'] * fwhm_max
     
     # find the images that need to be processed
     all_images = reduction_metadata.find_all_images(setup, reduction_metadata,
@@ -70,14 +72,16 @@ def run_stage5(setup):
         os.mkdir(diffim_directory_path)
 
     reduction_metadata.update_a_cell_to_layer('data_architecture', 0, 'KERNEL_PATH', kernel_directory_path)
-    # difference images are written for verbose
+    # difference images are written for verbosity level > 0 
     reduction_metadata.update_a_cell_to_layer('data_architecture', 0, 'DIFFIM_PATH', diffim_directory_path)	                                        
 	
+    #For a quick image subtraction, pre-calculate a sufficiently large u_matrix
+    #based on the largest FWHM and store it to disk -> needs config switch
+
     ref_extended, bright_mask = open_reference(setup, ref_image_directory, ref_image_name, kernel_size,
                    max_adu, ref_extension = 0, log = None)
 
-    #For a quick image subtraction, pre-calculate a sufficiently large u_matrix
-    #based on the largest FWHM and store it to disk -> needs config switch
+    umatrix = umatrix_construction(reference_image, ker_size, model_image=None)
 
     if len(new_images) > 0:
 
@@ -149,153 +153,6 @@ def run_stage5(setup):
 
     return status, report, reduction_metadata
 
-def open_an_image(setup, image_directory, image_name,
-                  image_index=0, log=None):
-   
-    image_directory_path = image_directory
-
-    logs.ifverbose(log, setup,
-                   'Attempting to open image ' + os.path.join(image_directory_path, image_name))
-
-    try:
-
-        image_data = fits.open(os.path.join(image_directory_path, image_name),
-                               mmap=True)
-        image_data = image_data[image_index]
-
-        logs.ifverbose(log, setup, image_name + ' open : OK')
-
-        return image_data
-
-    except:
-        logs.ifverbose(log, setup, image_name + ' open : not OK!')
-
-        return None
-
-def open_reference(setup, ref_image_directory, ref_image_name, kernel_size,
-                   max_adu, ref_extension = 0, log = None):
-    '''
-    reading difference image for constructing u matrix
-
-    :param object string: reference imagefilename
-    :param object string: reference image filename
-    :param object integer: kernel size edge length of the kernel in px
-    :param object float: index of the maximum adu values
-    :return: images, mask
-    '''
-
-    logs.ifverbose(log, setup,
-                   'Attempting to open ref image ' + os.path.join(ref_directory_path, ref_image_name))
-
-    ref_image = fits.open(os.path.join(ref_image_directory_path, ref_image_name), mmap=True)
-
-	#increase kernel size by 2 and define circular mask
-    kernel_size_plus = kernel_size + 2
-    mask_kernel = np.ones(kernel_size_plus * kernel_size_plus, dtype=float)
-    mask_kernel = mask_kernel.reshape((kernel_size_plus, kernel_size_plus))
-    xyc = kernel_size_plus / 2
-    radius_square = (xyc)**2
-    for idx in range(kernel_size_plus):
-        for jdx in range(kernel_size_plus):
-            if (idx - xyc)**2 + (jdx - xyc)**2 >= radius_square:
-                mask_kernel[idx, jdx] = 0.
-
-    #subtract background estimate using 10% percentile
-    ref10pc = np.percentile(ref_image[ref_extension].data, 10.)
-    ref_image[ref_extension].data = ref_image[ref_extension].data - \
-        np.percentile(ref_image[ref_extension].data, 10.)
-
-    logs.ifverbose(log, setup,
-                   'Background reference= ' + str(ref10pc))
-
-    # extend image size for convolution and kernel solution
-    ref_extended = np.zeros((np.shape(ref_image[ref_image_extension].data)[
-                            0] + 2 * kernel_size, np.shape(ref_image[ref_image_extension].data)[1] + 2 * kernel_size))
-    ref_extended[kernel_size:-kernel_size, kernel_size:-
-                 kernel_size] = np.array(ref_image[ref_image_extension].data, float)
-    
-    #apply consistent mask
-    ref_bright_mask = ref_extended > max_adu + ref10pc
-    mask_propagate[ref_bright_mask] = 1.
-    #increase mask size to kernel size
-    mask_propagate = convolve2d(mask_propagate, mask_kernel, mode='same')
-    bright_mask = mask_propagate > 0.
-    ref_extended[bright_mask] = 0.
-
-    return ref_extended, bright_mask
-
-
-def open_images(setup, ref_image_directory, data_image_directory, ref_image_name,
-                data_image_name, kernel_size, max_adu, ref_extension = 0, 
-                data_image_extension = 1, log = None):
-	#to be updated with open_an_image ....
-    '''
-    Reference and data image needs to be opened jointly and bright pixels
-    are masked on both images depending on the corresponding kernel size
-    and max_adu
-
-    :param object string: reference imagefilename
-    :param object string: data image filename
-    :param object string: reference image filename
-    :param object string: data image filename
-    :param object integer: kernel size edge length of the kernel in px
-    :param object float: index of the maximum adu values
-    :return: images, mask
-    '''
-
-    logs.ifverbose(log, setup,
-                   'Attempting to open data image ' + os.path.join(data_image_directory_path, data_image_name))
-
-    data_image = fits.open(os.path.join(data_image_directory_path, data_image_name), mmap=True)
-
-    logs.ifverbose(log, setup,
-                   'Attempting to open ref image ' + os.path.join(ref_directory_path, ref_image_name))
-
-    ref_image = fits.open(os.path.join(ref_image_directory_path, ref_image_name), mmap=True)
-
-	#increase kernel size by 2 and define circular mask
-    kernel_size_plus = kernel_size + 2
-    mask_kernel = np.ones(kernel_size_plus * kernel_size_plus, dtype=float)
-    mask_kernel = mask_kernel.reshape((kernel_size_plus, kernel_size_plus))
-    xyc = kernel_size_plus / 2
-    radius_square = (xyc)**2
-    for idx in range(kernel_size_plus):
-        for jdx in range(kernel_size_plus):
-            if (idx - xyc)**2 + (jdx - xyc)**2 >= radius_square:
-                mask_kernel[idx, jdx] = 0.
-
-    #subtract background estimate using 10% percentile
-    ref10pc = np.percentile(ref_image[ref_extension].data, 10.)
-    ref_image[ref_extension].data = ref_image[ref_extension].data - \
-        np.percentile(ref_image[ref_extension].data, 10.)
-
-    logs.ifverbose(log, setup,
-                   'Background reference= ' + str(ref10pc))
-
-    # extend image size for convolution and kernel solution
-    data_extended = np.zeros((np.shape(data_image[data_image_extension].data)[
-                             0] + 2 * kernel_size, np.shape(data_image[data_image_extension].data)[1] + 2 * kernel_size))
-    ref_extended = np.zeros((np.shape(ref_image[ref_image_extension].data)[
-                            0] + 2 * kernel_size, np.shape(ref_image[ref_image_extension].data)[1] + 2 * kernel_size))
-    data_extended[kernel_size:-kernel_size, kernel_size:-
-                  kernel_size] = np.array(data_image[data_image_extension].data, float)
-    ref_extended[kernel_size:-kernel_size, kernel_size:-
-                 kernel_size] = np.array(ref_image[ref_image_extension].data, float)
-    
-    #apply consistent mask
-    ref_bright_mask = ref_extended > max_adu + ref10pc
-    data_bright_mask = data_extended > max_adu
-    mask_propagate = np.zeros(np.shape(data_extended))
-    mask_propagate[ref_bright_mask] = 1.
-    mask_propagate[data_bright_mask] = 1.
-    #increase mask size to kernel size
-    mask_propagate = convolve2d(mask_propagate, mask_kernel, mode='same')
-    bright_mask = mask_propagate > 0.
-    ref_extended[bright_mask] = 0.
-    data_extended[bright_mask] = 0.
-
-    return ref_extended, data_extended, bright_mask
-
 def noise_model(model_image, gain, readout_noise, flat=None, initialize=None):
 
     noise_image = np.copy(model_image)
@@ -324,6 +181,11 @@ def umatrix_constant(reference_image, ker_size, model_image=None):
 
     :return: u matrix
     '''
+    try:
+        from umatrix_routine import umatrix_construction, umatrix_bvector_construction, bvector_construction
+    except ImportError:
+        print('cannot import cython module umatrix_routine')
+        return []
 
     if ker_size:
         if ker_size % 2 == 0:
@@ -331,7 +193,7 @@ def umatrix_constant(reference_image, ker_size, model_image=None):
         else:
             kernel_size = ker_size
 
-    weights = np.ones(np.shape(refernce_image))
+    weights = np.ones(np.shape(reference_image))
 
     # Prepare/initialize indices, vectors and matrices
     pandq = []
@@ -342,9 +204,9 @@ def umatrix_constant(reference_image, ker_size, model_image=None):
         for midx in range(kernel_size):
             pandq.append((lidx - half_kernel_size, midx - half_kernel_size))
 
-    u_matrix = umatrix_construction(reference_image, data_image, weights, pandq, n_kernel, kernel_size)
+    u_matrix = umatrix_construction(reference_image, weights, pandq, n_kernel, kernel_size)
 
-    return u_matrix, b_vector
+    return u_matrix
 
 def kernel_preparation_matrix(data_image, reference_image, ker_size, model_image=None):
     '''
@@ -395,18 +257,20 @@ def kernel_preparation_matrix(data_image, reference_image, ker_size, model_image
 
 def kernel_solution(u_matrix, b_vector, kernel_size):
     '''
-    reshape kernel solution for convolution
+    reshape kernel solution for convolution and obtain uncertainty 
+    from lstsq solution
 
     :param object array: u_matrix
 	:param object array: b_vector
-
     :return: kernel matrix
     '''
+    #For better stability: solve the least square problem via np.linalg.lstsq
+    #inv_umatrix = np.linalg.inv(u_matrix)
+    #a_vector = np.dot(inv_umatrix, b_vector)
+    a_vector = np.linalg.lstsq(u_matrix,b_vector)[0]
+	#a_vector_err = MSE*np.diagonal(np.matrix(np.dot(u_matrix.T, u_matrix)).I)
+    #MSE: mean square error of the residuals
 
-    inv_umatrix = np.linalg.inv(u_matrix)
-    a_vector = np.dot(inv_umatrix, b_vector)
-
-    #    kernel_no_back = kernel_image[0:len(kernel_image)-1]
     output_kernel = np.zeros(kernel_size * kernel_size, dtype=float)
     output_kernel = a_vector[:-1]
     output_kernel = output_kernel.reshape((kernel_size, kernel_size))
@@ -456,40 +320,6 @@ def difference_image_single_iteration(ref_imagename, data_imagename, kernel_size
 
     return difference_image, output_kernel, a_vector[-1]
 
-def read_images_for_substamps(ref_image_filename, data_image_filename, kernel_size, max_adu):
-    data_image = fits.open(data_image_filename)
-    ref_image = fits.open(ref_image_filename)
-    # Masks
-    kernel_size_plus = kernel_size
-    mask_kernel = np.ones(kernel_size_plus * kernel_size_plus, dtype=float)
-    mask_kernel = mask_kernel.reshape((kernel_size_plus, kernel_size_plus))
-    xyc = kernel_size_plus / 2
-    radius_square = (xyc)**2
-    for idx in range(kernel_size_plus):
-        for jdx in range(kernel_size_plus):
-            if (idx - xyc)**2 + (jdx - xyc)**2 >= radius_square:
-                mask_kernel[idx, jdx] = 0.
-    ref10pc = np.percentile(ref_image[0].data, 10)
-    ref_image[0].data = ref_image[0].data - \
-        np.percentile(ref_image[0].data, 0.1)
-
-    # extend image size for convolution and kernel solution
-    data_extended = np.zeros((np.shape(data_image[1].data)))
-    ref_extended = np.zeros((np.shape(ref_image[0].data)))
-    data_extended = np.array(data_image[1].data, float)
-    ref_extended = np.array(ref_image[0].data, float)
-
-    ref_bright_mask = ref_extended > max_adu + ref10pc
-    data_bright_mask = data_extended > max_adu
-    mask_propagate = np.zeros(np.shape(data_extended))
-    mask_propagate[ref_bright_mask] = 1.
-    mask_propagate[data_bright_mask] = 1.
-    mask_propagate = convolve2d(mask_propagate, mask_kernel, mode='same')
-    bright_mask = mask_propagate > 0.
-    ref_extended[bright_mask] = 0.
-    data_extended[bright_mask] = 0.
-
-    return ref_extended, data_extended, bright_mask
 
 def difference_image_subimages(ref_imagename, data_imagename,
     kernel_size, subimage_shape, overlap=0., mask=None, max_adu=np.inf):
