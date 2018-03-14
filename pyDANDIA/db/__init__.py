80bd068757836f1175c323249163562790270888
Markus Demleitner <m@tfiu.de>
2018-03-14 16:46:42 +0100

    Un-tabbifying db sources.

--- pyDANDIA/db/__init__.py
+++ pyDANDIA/db/__init__.py
@@ -1,5 +1,5 @@
 from .common import (get_connection, 
-	feed_exposure, 
-	feed_to_table, feed_to_table_many_dict, feed_to_table_many)
+    feed_exposure, 
+    feed_to_table, feed_to_table_many_dict, feed_to_table_many)
 
 from . astropy_interface import load_astropy_table, query_to_astropy_table
--- pyDANDIA/db/astropy_interface.py
+++ pyDANDIA/db/astropy_interface.py
@@ -10,29 +10,29 @@ from . import common
 
 
 def load_astropy_table(conn, db_table_name, table):
-	"""ingests the astropy table table into db_table_name via conn.
-	"""
-	common.feed_to_table_many(
-		conn,
-		db_table_name,
-		table.colnames,
-		[tuple(r) for r in table])
+    """ingests the astropy table table into db_table_name via conn.
+    """
+    common.feed_to_table_many(
+        conn,
+        db_table_name,
+        table.colnames,
+        [tuple(r) for r in table])
 
 
 def query_to_astropy_table(conn, query, args=()):
-	"""tries to come up with a reasonable astropy table for a database
-	query result.
-	"""
-	cursor = conn.cursor()
-	cursor.execute(query, args)
-	keys = [cd[0] for cd in cursor.description]
-	tuples = list(cursor)
+    """tries to come up with a reasonable astropy table for a database
+    query result.
+    """
+    cursor = conn.cursor()
+    cursor.execute(query, args)
+    keys = [cd[0] for cd in cursor.description]
+    tuples = list(cursor)
 
-	def getColumn(index):
-		return [t[index] for t in tuples]
+    def getColumn(index):
+        return [t[index] for t in tuples]
 
-	data = [
-		table.Column(name=k,
-			data=getColumn(i))
-		for i,k in enumerate(keys)]
-	return table.Table(data=data)
+    data = [
+        table.Column(name=k,
+            data=getColumn(i))
+        for i,k in enumerate(keys)]
+    return table.Table(data=data)
