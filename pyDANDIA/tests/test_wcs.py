72855f85a5dc260d45079db0e895835fa422f7cd
rachel3834 <rstreet@lco.global>
2018-12-10 15:58:45 -0800

    Switched from using the initial WCS-fit CD parameters (which are not always available or have invalid entries) to using the pixel scale value from the header

--- pyDANDIA/tests/test_wcs.py
+++ pyDANDIA/tests/test_wcs.py
@@ -59,6 +59,19 @@ def test_search_vizier_for_objects_in_fov():
     catalog = wcs.search_vizier_for_objects_in_fov(header['RA'], header['Dec'], radius)
 
     assert len(catalog) == 50
+
+def test_search_vizier_for_2mass_sources():
+    """Function to test the online extraction of a catalogue of known
+    sources within a given field of view"""
+    
+    ra = '270.8210141'
+    dec = '-26.909889'
+    radius = 2027*0.467*60.0/2.0
+    catalog = wcs.search_vizier_for_2mass_sources(ra, dec, radius)
+
+    assert len(catalog) > 0
+
     
 if __name__ == '__main__':
-    test_reference_astrometry()
+    #test_reference_astrometry()
+    test_search_vizier_for_2mass_sources()
