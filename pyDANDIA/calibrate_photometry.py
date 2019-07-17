BUG FIX in stage6 calculation of photometric uncertainties; continued debugging at runtime of catalog upgrades to calibrate_photometry and stage7

pyDANDIA/calibrate_photometry.py
@@ -252,30 +252,20 @@ def select_calibration_stars(star_catalog,params,log):
         else:
             idx = list(set(idx).intersection(jdx))
     
-    print(idx)
     star_catalog['clean'][idx] = 1.0
-    
-    print(star_catalog['clean'])
     
     log.info('Selected '+str(len(idx))+\
             ' stars with VPHAS+ data suitable for use in photometric calibration')
     
     # Now selecting stars with good quality photometry from the ROME data and
     # Gaia positional data:
-    print(len(np.where(star_catalog['clean'] == 1.0)[0]))
     idx0 = np.where(star_catalog['clean'] == 1.0)[0].tolist()
     idx1 = np.where(star_catalog['mag'] > 10.0)[0].tolist()
     idx2 = np.where(star_catalog['mag_err'] > 0.0)[0].tolist()
     idx3 = np.where(star_catalog['gaia_ra'] != 0.0)[0].tolist()
-    print(len(idx0))
-    print(len(idx1))
-    print(len(idx2))
-    print(len(idx3))
     idx = set(idx0).intersection(set(idx1))
     idx = idx.intersection(set(idx2))
-    idx = list(idx.intersection(set(idx3)))
-    print(len(idx))
-    exit()
+    idx = idx.intersection(set(idx3))
     
     log.info('Of these, identified '+str(len(list(idx)))+' detected stars with good photometry')
     
@@ -291,14 +281,17 @@ def select_calibration_stars(star_catalog,params,log):
     
     kdx = list(idx.intersection(set(jdx[0])))
     
-    if len(jdx[0]) < 100:
+    if len(jdx[0]) < 100 or len(kdx) < 100:
         kdx = list(idx)
         
         log.info('WARNING: Could not selected on position; too few detected stars with good photometry around target location')
         
+        kdx = list(idx)
+    
+    star_catalog['clean'] = np.zeros(len(star_catalog['clean']))
     star_catalog['clean'][kdx] = 1.0
 
-    log.info('Identified '+str(len(kdx))+' detected stars with good photometry')
+    log.info('Selected '+str(len(kdx))+' stars with good detected and catalog photometry')
     
     return star_catalog
     
@@ -350,11 +343,16 @@ def extract_matched_stars_index(star_catalog,log):
     
     if len(ddx) == 0:
         raise ValueError('Insufficient matched stars to continue photometric calibration')
+    else:
+        log.info('Using '+str(len(ddx))+' in photometric calibration')
         
-    match_index = np.zeros(len(ddx,2))
+    match_index = np.zeros([len(ddx),2])
     
-    match_index[:,0] = ddx
-    match_index[:,1] = ddx
+    print(ddx)
+    
+    match_index[:,0] = ddx.astype('int')
+    match_index[:,1] = ddx.astype('int')
+    print(match_index)
     
     if len(match_index) > 0:
         log.info('Matched '+str(len(match_index)))

pyDANDIA/photometry.py
@@ -7,6 +7,8 @@ Created on Wed Oct 18 15:42:26 2017
 import os
 import sys
 import numpy as np
+from astropy.stats import SigmaClip
+from photutils import StdBackgroundRMS
 from photutils import aperture_photometry
 from photutils import CircularAperture
 from photutils.utils import calc_total_error
@@ -475,7 +477,10 @@ def run_psf_photometry_on_difference_image(setup, reduction_metadata, log, ref_s
                  log.info('Image not photometered because of zeroed FWHM measurements')
         
         return use_image
-
+    
+    def null_background(x,axis=None):
+        return 0
+        
     psf_diameter = reduction_metadata.psf_dimensions[1]['psf_radius'][0]*2.0
     half_psf = int(psf_diameter)
 
@@ -527,9 +532,16 @@ def run_psf_photometry_on_difference_image(setup, reduction_metadata, log, ref_s
     use_image = check_fwhm(reduction_metadata, image_id, log)
     
     if use_image:
+        
         radius = psf_diameter/2.0
         apertures = CircularAperture(positions, r=radius)
-        error = calc_total_error(difference_image, np.std(difference_image.ravel()), 1)
+        
+        sigma_clip = SigmaClip(sigma=3.,cenfunc=null_background)
+        bkgrms = StdBackgroundRMS(sigma_clip)
+        bkgrms_value1 = bkgrms.calc_background_rms(difference_image)
+        error = calc_total_error(difference_image, bkgrms_value1, 1)
+        #error = calc_total_error(difference_image, np.std(difference_image.ravel()), 1)
+
         try:
             phot_table = aperture_photometry(difference_image, apertures, method='subpixel',
                                          error=error)
@@ -636,7 +648,7 @@ def run_psf_photometry_on_difference_image(setup, reduction_metadata, log, ref_s
                     
                     flux = phot_table[j][3]/phot_scale_factor
                     flux_err = phot_table[j][4]
-                                        
+                    
                     flux_tot = ref_flux*ref_exposure_time - flux
                     flux_err_tot = (error_ref_flux ** 2*ref_exposure_time + flux_err**2/phot_scale_factor**2) ** 0.5
                         

pyDANDIA/stage7.py
@@ -39,13 +39,14 @@ def run_stage7(setup):
                                                               log)
         
             ref_image_list = list_reference_images_in_filter(conn,primary_ref,f,log)
-        
+            
             for ref_image in ref_image_list:
                 
                 ref_phot = extract_photometry_for_reference_image(conn,primary_ref,f,
                                                               ref_image['refimg_id'],
                                                               log)
-                (delta_mag, delta_mag_err) = calc_magnitude_offset(primary_phot, ref_phot,log)
+                       
+                (delta_mag, delta_mag_err) = calc_magnitude_offset(primary_phot, ref_phot, log)
                 
                 apply_magnitude_offset(conn, ref_phot, refimg_id, delta_mag, delta_mag_err, log)
                 
@@ -101,18 +102,20 @@ def identify_primary_reference_datasets(conn, log):
     return primary_ref
 
 def extract_photometry_for_reference_image(conn,primary_ref,f,refimg_id,log):
-    """Function to extract from the DB the photometry for the primary 
-    reference dataset in the given filter"""
+    """Function to extract from the DB the photometry for a specific 
+    reference image in the given filter"""
+    
+    log.info('Extracting photometry for stars in reference image '+str(refimg_id))
     
     query = 'SELECT phot_id, star_id, hjd, calibrated_mag, calibrated_mag_err, calibrated_flux, calibrated_flux_err FROM phot WHERE reference_image="'+str(refimg_id)+\
                 '" AND software="'+str(primary_ref['software_id'])+\
                 '" AND filter="'+str(primary_ref[f])+\
                 '" AND facility="'+str(primary_ref['facility_id'])+'"'
-    phot = phot_db.query_to_astropy_table(conn, query, args=())
+    ref_phot = phot_db.query_to_astropy_table(conn, query, args=())
     
-    log.info(' -> Extracted photometry for '+str(len(phot))+' stars for reference image '+str(refimg_id)+' and filter '+f)
+    log.info(' -> Extracted photometry for '+str(len(ref_phot))+' stars for reference image '+str(refimg_id)+' and filter '+f)
     
-    return primary_phot
+    return ref_phot
 
 def list_reference_images_in_filter(conn,primary_ref,f,log):
     """Function to identify all available datasets in the given 
@@ -122,12 +125,15 @@ def list_reference_images_in_filter(conn,primary_ref,f,log):
         QuerySet of matching reference_image entries
     """
     
+    log.info('Identifying all current reference image in filter '+str(f))
+    
     query = 'SELECT * FROM reference_images WHERE filter="'+str(primary_ref[f])+\
             '" AND software="'+str(primary_ref['software_id'])+\
             '" AND facility!="'+str(primary_ref['facility_id'])+'"'
     
     ref_image_list = phot_db.query_to_astropy_table(conn, query, args=())
-    print(ref_image_list)
+    
+    log.info(repr(ref_image_list))
     
     return ref_image_list
 
@@ -136,15 +142,21 @@ def calc_magnitude_offset(primary_phot, ref_phot, log):
     measured magnitudes of stars in the reference image of a dataset relative 
     to the primary reference dataset in that passband."""
 
+    log.info('Computing the magnitude offset between the primary and reference photometry')
+    print(primary_phot)
     numer = 0.0
     denom = 0.0    
-    for star in primary_phot['star_id']:
+    for j,star in enumerate(primary_phot['star_id']):
+        print(primary_phot[j])
+        mag_pri_ref = primary_phot['calibrated_mag'][j]
+        merr_pri_ref = primary_phot['calibrated_mag_err'][j]
         
-        mag_pri_ref = primary_phot['calibrated_mag'][star]
-        merr_pri_ref = primary_phot['calibrated_mag_err'][star]
-        mag_ref = ref_phot['calibrated_mag'][star]
-        merr_ref = ref_phot['calibrated_mag_err'][star]
+        jj = np.where(ref_phot['star_id'] == star)
         
+        mag_ref = ref_phot['calibrated_mag'][jj]
+        merr_ref = ref_phot['calibrated_mag_err'][jj]
+        
+        print(ref_phot[jj])
         sigma = np.sqrt(merr_pri_ref*merr_pri_ref + merr_ref*merr_ref)
         
         numer += (mag_pri_ref - mag_ref) / (sigma*sigma)
@@ -153,11 +165,15 @@ def calc_magnitude_offset(primary_phot, ref_phot, log):
     delta_mag = numer / denom
     delta_mag_err = 1.0 / denom
     
+    log.info(' -> Delta_mag = '+str(delta_mag)+', delta_mag_err = '+str(delta_mag_err))
+    
     return delta_mag, delta_mag_err
 
 def apply_magnitude_offset(conn, ref_phot, refimg_id, delta_mag, delta_mag_err, log):
     """Function to apply the calculated magnitude offset to ALL photometry
     calculated relative to the reference image in the database."""
+    
+    log.info('Applying the magnitude offset to all photometry calculated using reference image '+str(refimg_id))
     
     query = 'SELECT phot_id, star_id, hjd, calibrated_mag, calibrated_mag_err, calibrated_flux, calibrated_flux_err FROM phot WHERE reference_image="'+str(refimg_id)+'"'
     phot_data = phot_db.query_to_astropy_table(conn, query, args=())
@@ -184,3 +200,4 @@ def apply_magnitude_offset(conn, ref_phot, refimg_id, delta_mag, delta_mag_err, 
     cursor.executemany(command, values)
     
     conn.commit()
+    