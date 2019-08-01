2b7c297a7972d71dee7679f0dfc7ae0e2eca0e45
rachel3834 <rstreet@lco.global>
2019-07-31 18:27:39 -0700

    Debugging at runtime

--- pyDANDIA/stage7.py
+++ pyDANDIA/stage7.py
@@ -13,6 +13,7 @@ from pyDANDIA import metadata
 from pyDANDIA import logs
 from pyDANDIA import phot_db
 from pyDANDIA import photometry
+import numpy as np
 
 
 VERSION = 'pyDANDIA_stage7_v0.1'
@@ -33,21 +34,27 @@ def run_stage7(setup):
     stars_table = phot_db.fetch_stars_table(conn)
     log.info(' -> Extracted starlist of '+str(len(stars_table))+' objects')
     
+    cal_stars = select_calibration_star_sample(stars_table, log, 
+                                               primary_ref['refimg_id_ip'], 
+                                                sample_size=20000)
+    
     for f in ['ip', 'rp', 'gp']:
         
         if 'refimg_id_'+f in primary_ref.keys():
-            primary_phot = extract_photometry_for_reference_image(conn,primary_ref,stars_table,f,
+            primary_phot = extract_photometry_for_reference_image(conn,primary_ref,cal_stars,f,
                                                               primary_ref['refimg_id_'+f],
+                                                              primary_ref['facility_id'],
                                                               log)
-        
+            
             ref_image_list = list_reference_images_in_filter(conn,primary_ref,f,log)
             
             for ref_image in ref_image_list:
                 
-                ref_phot = extract_photometry_for_reference_image(conn,primary_ref,stars_table,f,
+                ref_phot = extract_photometry_for_reference_image(conn,primary_ref,cal_stars,f,
                                                               ref_image['refimg_id'],
+                                                              ref_image['facility'],
                                                               log)
-                       
+                
                 (delta_mag, delta_mag_err) = calc_magnitude_offset(primary_phot, ref_phot, log)
                 
                 apply_magnitude_offset(conn, ref_phot, refimg_id, delta_mag, delta_mag_err, log)
@@ -103,30 +110,72 @@ def identify_primary_reference_datasets(conn, log):
         
     return primary_ref
 
-def extract_photometry_for_reference_image(conn,primary_ref,stars_table,f,refimg_id,log):
+def select_calibration_star_sample(stars_table, log, refimg_id, sample_size=20000):
+    """Function to select a subset of the stars with catalogue photometry, 
+    since extracting all photometry from the DB is prohibitively slow.
+    
+    This function returns a dictionary of the index of stars in the stars_table
+    and the stars row entries.
+    """
+    
+    log.info('Selecting a sample of '+str(sample_size)+\
+              ' randomly selected stars with catalogue photometry in reference image '+\
+              str(refimg_id))
+    
+    cal_stars = {}
+    
+    trials = 0
+    
+    while trials < len(stars_table) and len(cal_stars) < sample_size:
+        j = np.random.randint(0, len(stars_table))
+        
+        trials += 1
+        
+        s = stars_table[j]
+        
+        if s['vphas_source_id'] != None:
+            
+            cal_stars[j] = s
+    
+    return cal_stars
+    
+def extract_photometry_for_reference_image(conn,primary_ref,cal_stars,f,
+                                           refimg_id,facility_id,log):
     """Function to extract from the DB the photometry for a specific 
     reference image in the given filter"""
     
-    log.info('Extracting photometry for stars in reference image '+str(refimg_id))
+    log.info('Extracting photometry for '+str(len(cal_stars))+\
+              ' randomly selected stars with photometry in reference image '+\
+              str(refimg_id))
     
-    ref_phot = None
+    ref_phot = Table()
     
-    for j,s in enumerate(stars_table):
+    for j in cal_stars.keys():
+        
+        s = cal_stars[j]
+        
         query = 'SELECT phot_id, star_id, hjd, calibrated_mag, calibrated_mag_err, calibrated_flux, calibrated_flux_err FROM phot WHERE reference_image="'+str(refimg_id)+\
                 '" AND image="'+str(refimg_id)+\
                 '" AND star_id="'+str(s['star_id'])+\
                 '" AND software="'+str(primary_ref['software_id'])+\
                 '" AND filter="'+str(primary_ref[f])+\
-                '" AND facility="'+str(primary_ref['facility_id'])+'"'
+                '" AND facility="'+str(facility_id)+'"'
+        
         star_phot = phot_db.query_to_astropy_table(conn, query, args=())
-                
-        if ref_phot == None and len(star_phot) > 0:
+        
+        if len(ref_phot) == 0 and len(star_phot) > 0 and star_phot['calibrated_mag'] != 0.0:
             ref_phot = star_phot
-        elif ref_phot != None and len(star_phot) > 0:
+        elif len(ref_phot) > 0 and len(star_phot) > 0:
             ref_phot = vstack([ref_phot, star_phot])
         
-    log.info(' -> Extracted photometry for '+str(len(ref_phot))+' stars for reference image '+str(refimg_id)+' and filter '+f)
+        if len(ref_phot)%1000 == 0:
+            log.info(' -> Extracted photometry for '+str(len(ref_phot))+' stars out of '+str(len(cal_stars)))
     
+    if len(ref_phot) > 0:
+        log.info(' -> Extracted photometry for '+str(len(ref_phot))+' stars for reference image '+str(refimg_id)+' and filter '+f)
+    else:
+        log.info(' -> No photometry available for reference image '+str(refimg_id)+' and filter '+f)
+        
     return ref_phot
 
 def list_reference_images_in_filter(conn,primary_ref,f,log):
@@ -146,7 +195,6 @@ def list_reference_images_in_filter(conn,primary_ref,f,log):
     ref_image_list = phot_db.query_to_astropy_table(conn, query, args=())
     
     log.info(repr(ref_image_list))
-    print(ref_image_list)
     
     return ref_image_list
 
@@ -156,26 +204,29 @@ def calc_magnitude_offset(primary_phot, ref_phot, log):
     to the primary reference dataset in that passband."""
 
     log.info('Computing the magnitude offset between the primary and reference photometry')
-    print(primary_phot)
-    exit()
+    
     numer = 0.0
     denom = 0.0
     for j,star in enumerate(primary_phot['star_id']):
-        print(primary_phot[j])
         mag_pri_ref = primary_phot['calibrated_mag'][j]
         merr_pri_ref = primary_phot['calibrated_mag_err'][j]
         
         jj = np.where(ref_phot['star_id'] == star)
         
-        mag_ref = ref_phot['calibrated_mag'][jj]
-        merr_ref = ref_phot['calibrated_mag_err'][jj]
-        
-        print(ref_phot[jj])
-        sigma = np.sqrt(merr_pri_ref*merr_pri_ref + merr_ref*merr_ref)
-        
-        numer += (mag_pri_ref - mag_ref) / (sigma*sigma)
-        denom += (1/sigma*sigma)
-    
+        if len(ref_phot[jj]) > 0 and merr_pri_ref != np.nan and merr_pri_ref > 0.0:
+            mag_ref = ref_phot['calibrated_mag'][jj][0]
+            merr_ref = ref_phot['calibrated_mag_err'][jj][0]
+            
+            if merr_ref != np.nan and merr_ref > 0.0:
+                sigma = np.sqrt(merr_pri_ref*merr_pri_ref + merr_ref*merr_ref)
+                
+                numer += (mag_pri_ref - mag_ref) / (sigma*sigma)
+                denom += (1/sigma*sigma)
+                
+                print('Sigma:',sigma)
+                print((mag_pri_ref - mag_ref), (sigma*sigma))
+                print(numer, denom)
+            
     delta_mag = numer / denom
     delta_mag_err = 1.0 / denom
     
