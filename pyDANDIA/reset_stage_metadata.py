f111d1bc402154826b4987ccf850d4a6dfe5159a
rachel3834 <rstreet@lco.global>
2020-02-10 12:13:50 -0800

    Reset based on previous stages' status

--- pyDANDIA/reset_stage_metadata.py
+++ pyDANDIA/reset_stage_metadata.py
@@ -3,14 +3,18 @@ import sys
 from pyDANDIA import metadata
 
 def reset_red_status_for_stage(red_dir,stage_number):
-
+    """Function to reset the reduction status table entries for a given stage.
+    Note that the status for all images will be set to zero (process) except
+    for entries that were flagged as -1 (do not process) by the PREVIOUS
+    stage"""
+    
     reduction_metadata = metadata.MetaData()
     reduction_metadata.load_all_metadata(red_dir, 'pyDANDIA_metadata.fits')
 
     stage_name = 'STAGE_'+str(stage_number)
 
     image_list = reduction_metadata.reduction_status[1]['IMAGES'].data
-    image_status = reduction_metadata.reduction_status[1]['STAGE_'+str(stage_number)].data
+    image_status = reduction_metadata.reduction_status[1]['STAGE_'+str(stage_number-1)].data
 
     for i,image in enumerate(image_list):
 
