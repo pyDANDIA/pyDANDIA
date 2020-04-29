a19d283bbfc1bf9154d3eb5ef4e9b041364fcef2
rachel3834 <rstreet@lco.global>
2020-04-28 20:12:59 -0700

    Test statements added

--- pyDANDIA/photometry.py
+++ pyDANDIA/photometry.py
@@ -592,6 +592,7 @@ def run_psf_photometry_on_difference_image(setup, reduction_metadata, log, ref_s
                          error=error)
             print(phot_table)
             print(error)
+            print(bkg.background, bkg.background_rms)
         except ValueError:
             import pdb;
             pdb.set_trace()
