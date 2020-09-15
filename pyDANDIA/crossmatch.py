ccecd8c972e636bc4dcc3be38e048953a817762c
rachel3834 <rstreet@lco.global>
2020-09-15 14:58:28 -0700

    Completed unittests for crossmatch

--- pyDANDIA/crossmatch.py
+++ pyDANDIA/crossmatch.py
@@ -1,34 +1,41 @@
 from astropy.io import fits
 from astropy.table import Table
 from astropy.table import Column
+from astropy.coordinates import SkyCoord
 from pyDANDIA import match_utils
+import numpy as np
+from os import path
 
 class CrossMatchTable():
     """Class describing the cross-identification of stars between multiple
     datasets"""
 
     def __init__(self):
-        self.header = None
+        self.datasets = None
         self.matched_stars = []
 
     def create(self, params):
         xmatch = fits.HDUList()
 
-        header = fits.Header()
-        header.update({'PRIMARY': params['primary_ref_dir']})
-        header.update({'PRIMFILT': params['primary_ref_filter']})
+        col_list = [ Column(name='primary_ref_dir', data=[params['primary_ref_dir']], dtype='str'),
+                     Column(name='primary_ref_filter', data=[params['primary_ref_filter']], dtype='str')]
+
         for i,red_dir in enumerate(params['red_dir_list']):
-            header.update({'DATASET'+str(i): red_dir})
-            header.update({'FILTER'+str(i): params['red_dataset_filters'][i]})
+            col_list.append( Column(name='dataset'+str(i), data=[red_dir], dtype='str') )
+            col_list.append( Column(name='filter'+str(i), data=[params['red_dataset_filters'][i]], dtype='str') )
+
             self.init_matched_stars_table()
 
-        self.header = header
+        self.datasets = Table(col_list)
 
     def add_dataset(self, red_dir, filter_name):
         idx = len(self.matched_stars)
-        self.header.update({'DATASET'+str(idx): red_dir})
-        self.header.update({'FILTER'+str(idx): filter_name})
+
+        self.datasets.add_column(Column(name='dataset'+str(idx), data=[red_dir], dtype='str'))
+        self.datasets.add_column(Column(name='filter'+str(idx), data=[filter_name], dtype='str'))
+
         self.init_matched_stars_table()
+
         return idx
 
     def init_matched_stars_table(self):
@@ -50,9 +57,9 @@ class CrossMatchTable():
         return it's index in the matched_stars table list, or -1 if not present"""
 
         idx = -1
-        for key, value in self.header:
-            if 'DATASET' in key and value == red_dir:
-                idx = int(key.replace('DATASET',''))
+        for col in self.datasets.colnames:
+            if 'dataset' in col and self.datasets[col][0] == red_dir:
+                idx = int(col.replace('dataset',''))
 
         return idx
 
@@ -71,29 +78,33 @@ class CrossMatchTable():
 
     def save(self, file_path):
         """Output crossmatch table to file"""
+        hdr = fits.Header()
+        hdr['NAME'] = 'Crossmatch table'
 
-        hdu_list = [fits.PrimaryHDU(self.header)]
-        hdu_list.append(fits.BinTableHDU(x, name='MATCH_TABLE_'+str(i)) for i,x in enumerate(self.matched_stars))
+        hdu_list = [fits.PrimaryHDU(header=hdr), fits.BinTableHDU(self.datasets, name='DATASETS')]
+        for i,x in enumerate(self.matched_stars):
+            hdu_list.append( fits.BinTableHDU(x, name='match_table_'+str(i)) )
+
         hdu_list = fits.HDUList(hdu_list)
         hdu_list.writeto(file_path, overwrite=True)
 
 
-    def load_matched_stars(self, dataset_idx):
+    def load_matched_stars(self, binary_table):
         """Method to load the matched_stars list"""
 
         matched_stars = match_utils.StarMatchIndex()
 
-        matched_stars.cat1_index = list(self.matched_stars[dataset_idx]['field_star_id'])
-        matched_stars.cat1_ra = list(self.matched_stars[dataset_idx]['field_ra'])
-        matched_stars.cat1_dec = list(self.matched_stars[dataset_idx]['field_dec'])
-        matched_stars.cat1_x = list(self.matched_stars[dataset_idx]['field_x'])
-        matched_stars.cat1_y = list(self.matched_stars[dataset_idx]['field_y'])
-        matched_stars.cat2_index = list(self.matched_stars[dataset_idx]['dataset_star_id'])
-        matched_stars.cat2_ra = list(self.matched_stars[dataset_idx]['dataset_ra'])
-        matched_stars.cat2_dec = list(self.matched_stars[dataset_idx]['dataset_dec'])
-        matched_stars.cat2_x = list(self.matched_stars[dataset_idx]['dataset_x'])
-        matched_stars.cat2_y = list(self.matched_stars[dataset_idx]['dataset_y'])
-        matched_stars.separation = list(self.matched_stars[dataset_idx]['separation'])
+        matched_stars.cat1_index = list(binary_table.data['field_star_id'])
+        matched_stars.cat1_ra = list(binary_table.data['field_ra'])
+        matched_stars.cat1_dec = list(binary_table.data['field_dec'])
+        matched_stars.cat1_x = list(binary_table.data['field_x'])
+        matched_stars.cat1_y = list(binary_table.data['field_y'])
+        matched_stars.cat2_index = list(binary_table.data['dataset_star_id'])
+        matched_stars.cat2_ra = list(binary_table.data['dataset_ra'])
+        matched_stars.cat2_dec = list(binary_table.data['dataset_dec'])
+        matched_stars.cat2_x = list(binary_table.data['dataset_x'])
+        matched_stars.cat2_y = list(binary_table.data['dataset_y'])
+        matched_stars.separation = list(binary_table.data['separation'])
         matched_stars.n_match = len(matched_stars.cat1_index)
 
         return matched_stars
@@ -105,7 +116,7 @@ class CrossMatchTable():
             raise IOError('Cannot find cross-match table at '+file_path)
 
         hdu_list = fits.open(file_path, mmap=True)
-        self.header = hdu_list.header
-
-        for extn in hdu_list[1:]:
-            self.matched_stars.append(self.load_matched_stars(extn))
+        self.datasets = hdu_list[1]
+        
+        for table_extn in hdu_list[2:]:
+            self.matched_stars.append(self.load_matched_stars(table_extn))
--- pyDANDIA/tests/test_crossmatch.py
+++ pyDANDIA/tests/test_crossmatch.py
@@ -1,5 +1,7 @@
+from os import path, remove
 from pyDANDIA import crossmatch
 from pyDANDIA import crossmatch_datasets
+from pyDANDIA import match_utils
 
 def test_create():
     params = {'primary_ref_dir': '/Users/rstreet1/OMEGA/test_data/primary_ref_dataset/',
@@ -33,7 +35,102 @@ def test_add_dataset():
     xmatch.add_dataset('/Users/rstreet1/OMEGA/test_data/non_ref_dataset2/', 'rp')
     assert(len(xmatch.matched_stars) == 2)
 
+def test_dataset_index():
+
+    params = {'primary_ref_dir': '/Users/rstreet1/OMEGA/test_data/primary_ref_dataset/',
+              'primary_ref_filter': 'ip',
+              'red_dir_list': [ '/Users/rstreet1/OMEGA/test_data/non_ref_dataset/' ],
+              'red_dataset_filters': [ 'rp' ],
+              'file_path': 'crossmatch_table.fits'}
+
+    xmatch = crossmatch.CrossMatchTable()
+    xmatch.create(params)
+
+    dataset_idx = xmatch.dataset_index(params['red_dir_list'][0])
+    assert(dataset_idx == 0)
+
+def test_update_matched_stars_table():
+
+    params = {'primary_ref_dir': '/Users/rstreet1/OMEGA/test_data/primary_ref_dataset/',
+              'primary_ref_filter': 'ip',
+              'red_dir_list': [ '/Users/rstreet1/OMEGA/test_data/non_ref_dataset/' ],
+              'red_dataset_filters': [ 'rp' ],
+              'file_path': 'crossmatch_table.fits'}
+
+    xmatch = crossmatch.CrossMatchTable()
+    xmatch.create(params)
+
+    matched_stars = match_utils.StarMatchIndex()
+    p = {'cat1_index': 1,
+         'cat1_ra': 270.0,
+         'cat1_dec': -18.0,
+         'cat1_x': 1000.0,
+         'cat1_y': 1000.0,
+         'cat2_index': 1,
+         'cat2_ra': 270.0,
+         'cat2_dec': -18.0,
+         'cat2_x': 1000.0,
+         'cat2_y': 1000.0,
+         'separation': 0.0}
+
+    matched_stars.add_match(p)
+
+    xmatch.update_matched_stars_table(0, matched_stars)
+
+    assert len(xmatch.matched_stars[0]) == 1
+
+def test_save():
+
+    params = {'primary_ref_dir': '/Users/rstreet1/OMEGA/test_data/primary_ref_dataset/',
+              'primary_ref_filter': 'ip',
+              'red_dir_list': [ '/Users/rstreet1/OMEGA/test_data/non_ref_dataset/' ],
+              'red_dataset_filters': [ 'rp' ],
+              'file_path': 'data/crossmatch_table.fits'}
+
+    if path.isfile(params['file_path']):
+      remove(params['file_path'])
+
+    xmatch = crossmatch.CrossMatchTable()
+    xmatch.create(params)
+
+    matched_stars = match_utils.StarMatchIndex()
+    p = {'cat1_index': 1,
+         'cat1_ra': 270.0,
+         'cat1_dec': -18.0,
+         'cat1_x': 1000.0,
+         'cat1_y': 1000.0,
+         'cat2_index': 1,
+         'cat2_ra': 270.0,
+         'cat2_dec': -18.0,
+         'cat2_x': 1000.0,
+         'cat2_y': 1000.0,
+         'separation': 0.0}
+
+    matched_stars.add_match(p)
+
+    xmatch.update_matched_stars_table(0, matched_stars)
+
+    xmatch.save(params['file_path'])
+
+    assert(path.isfile(params['file_path']))
+
+def test_load():
+    params = {'primary_ref_dir': '/Users/rstreet1/OMEGA/test_data/primary_ref_dataset/',
+              'primary_ref_filter': 'ip',
+              'red_dir_list': [ '/Users/rstreet1/OMEGA/test_data/non_ref_dataset/' ],
+              'red_dataset_filters': [ 'rp' ],
+              'file_path': 'data/crossmatch_table.fits'}
+
+    xmatch = crossmatch.CrossMatchTable()
+    xmatch.load(params['file_path'])
+
+    assert(xmatch.datasets != None)
+    assert(len(xmatch.matched_stars) > 0)
+
 if __name__ == '__main__':
     test_create()
     test_add_dataset()
-    
+    test_dataset_index()
+    test_update_matched_stars_table()
+    test_save()
+    test_load()
