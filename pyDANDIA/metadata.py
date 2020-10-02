c1126277ec95b68a9165fe81e4ce8b0a6ff904f6
rachel3834 <rstreet@lco.global>
2020-10-02 13:40:03 -0700

    Added debugging output

--- pyDANDIA/metadata.py
+++ pyDANDIA/metadata.py
@@ -1065,12 +1065,12 @@ class MetaData:
                           Column(name='ra', data=self.star_catalog[1]['ra'][idx]),
                           Column(name='dec', data=self.star_catalog[1]['dec'][idx]),
                           Column(name='separation', data=separations[idx]) ])
-        print(idx)
-        if len(idx) == 0:
+        
+        if len(idx[0]) == 0:
             print('Nearest closest star: ')
             idx = np.where(separations.value <= separations.value.min())
             print(self.star_catalog[1]['index'][idx], self.star_catalog[1]['ra'][idx], self.star_catalog[1]['dec'][idx], separations[idx])
-            
+
         return results
 
     def fetch_reduction_filter(self):
