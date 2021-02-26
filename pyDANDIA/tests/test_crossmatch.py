Added test code for xmatch with gaia catalog and fixed off-by-one index bug

pyDANDIA/tests/test_crossmatch.py
@@ -4,7 +4,8 @@ from pyDANDIA import crossmatch_datasets
 from pyDANDIA import match_utils
 from pyDANDIA import metadata
 from pyDANDIA import logs
-from astropy.table import Table
+from astropy.table import Table, Column
+from astropy import units as u
 import numpy as np
 
 def test_params():
@@ -14,7 +15,9 @@ def test_params():
                             'dataset1' : [ 'non_ref', '/Users/rstreet1/OMEGA/test_data/non_ref_dataset1/', 'none' ],
                             'dataset2' : [ 'non_ref', '/Users/rstreet1/OMEGA/test_data/non_ref_dataset1/', 'none' ]},
               'file_path': 'crossmatch_table.fits',
-              'log_dir': '.'}
+              'log_dir': '.',
+              'gaia_dr': 'Gaia_DR2',
+              'separation_threshold': (2.0/3600.0)*u.deg}
 
     return params
 
@@ -49,6 +52,65 @@ def test_orphans():
          'separation': -1.0}
     orphans.add_match(p)
     return orphans
+
+def test_field_index(xmatch):
+
+    xmatch.field_index.add_row([1,267.61861696019145, -29.829605383706895, 4, 1, None, 1, 0, 0, 0])
+    xmatch.field_index.add_row([2,267.70228408545813, -29.83032824102953, 4, 2, None, 2, 0, 0, 0])
+    xmatch.field_index.add_row([3,267.9873108673885, -29.829734325692858, 3, 1, None, 3, 0, 0, 0])
+    xmatch.field_index.add_row([4,267.9585073984874, -29.83002538112054, 3, 2, None, 4, 0, 0, 0])
+    xmatch.field_index.add_row([5,267.9623466389135, -29.82994179424344, 3, 3, None, 5, 0, 0, 0])
+    xmatch.field_index.add_row([6,267.943683356543, -29.830113202355186, 3, 4, None, 6, 0, 0, 0])
+    xmatch.field_index.add_row([7,267.90449275089594, -29.830465810573223, 3, 5, None, 7, 0, 0, 0])
+    xmatch.field_index.add_row([8,267.9504950018423, -29.830247462548577, 3, 6, None, 8, 0, 0, 0])
+    xmatch.field_index.add_row([9,267.9778110411362, -29.83012645385565, 3, 7, None, 9, 0, 0, 0])
+    xmatch.field_index.add_row([10,267.7950771349625, -29.830849947501875, 4, 3, None, 10, 0, 0, 0])
+    xmatch.field_index.add_row([11,268.06583501505446, -29.83070761362742, 3, 84, 4056397427727492224, 11, 0, 0, 0])
+    xmatch.field_index.add_row([12,268.0714302057775, -29.830599528895256, 3, 85, 4056403303242709888, 12, 0, 0, 0])
+    xmatch.field_index.add_row([13,268.07569655803013, -29.83064274854432, 3, 86, 4056403307573006208, 13, 0, 0, 0])
+    xmatch.field_index.add_row([14,268.07663104709775, -29.830575490772073, 3, 87, 4056403303204313344, 14, 0, 0, 0])
+    xmatch.field_index.add_row([15,268.07816636284224, -29.830684523662065, 3, 88, 4056403307572525184, 15, 0, 0, 0])
+
+    return xmatch
+
+def test_gaia_catalog():
+    nstars = 5
+    table_data = [  Column(name='source_id', data = np.array([4056397427727492224, 4056403303242709888, 4056403307573006208, 4056403307618099840, 4056403307572525184])),
+                    Column(name='ra', data = np.array([268.0657435285, 268.07133070742, 268.07560517009, 268.07658092476, 268.07819547689])),
+                    Column(name='ra_error', data = np.array([1.023, 0.1089, 0.4107, 38.0801, 2.0125])),
+                    Column(name='dec', data = np.array([-29.83088151459, -29.83073507254, -29.83081220358, -29.83107857205, -29.83092724519])),
+                    Column(name='dec_error', data = np.array([0.9754, 0.0946, 0.3265, 17.3181, 1.3702])) ]
+
+    colnames = ['phot_g_mean_flux', 'phot_g_mean_flux_error', 'phot_bp_mean_flux', 'phot_bp_mean_flux_error',
+                'phot_rp_mean_flux', 'phot_rp_mean_flux_error', 'proper_motion', 'pm_ra', 'pm_dec',
+                'parallax', 'parallax_error']
+
+    for col in colnames:
+        table_data.append( Column(name=col, data=np.zeros(nstars)) )
+
+    gaia_star_field_ids = [ 11, 12, 13, 14, 15 ]
+
+    return Table(table_data), gaia_star_field_ids
+
+def test_stars_table(xmatch):
+
+    xmatch.stars.add_row([1,267.61861696019145, -29.829605383706895]+[0.0]*36)
+    xmatch.stars.add_row([2,267.70228408545813, -29.83032824102953]+[0.0]*36)
+    xmatch.stars.add_row([3,267.9873108673885, -29.829734325692858]+[0.0]*36)
+    xmatch.stars.add_row([4,267.9585073984874, -29.83002538112054]+[0.0]*36)
+    xmatch.stars.add_row([5,267.9623466389135, -29.82994179424344]+[0.0]*36)
+    xmatch.stars.add_row([6,267.943683356543, -29.830113202355186]+[0.0]*36)
+    xmatch.stars.add_row([7,267.90449275089594, -29.830465810573223]+[0.0]*36)
+    xmatch.stars.add_row([8,267.9504950018423, -29.830247462548577]+[0.0]*36)
+    xmatch.stars.add_row([9,267.9778110411362, -29.83012645385565]+[0.0]*36)
+    xmatch.stars.add_row([10,267.7950771349625, -29.830849947501875]+[0.0]*36)
+    xmatch.stars.add_row([11,268.06583501505446, -29.83070761362742]+[0.0]*36)
+    xmatch.stars.add_row([12,268.0714302057775, -29.830599528895256]+[0.0]*36)
+    xmatch.stars.add_row([13,268.07569655803013, -29.83064274854432]+[0.0]*36)
+    xmatch.stars.add_row([14,268.07663104709775, -29.830575490772073]+[0.0]*36)
+    xmatch.stars.add_row([15,268.07816636284224, -29.830684523662065]+[0.0]*36)
+
+    return xmatch
 
 def test_metadata():
     dataset_metadata = metadata.MetaData()
@@ -230,6 +292,28 @@ def test_cone_search():
     assert(len(idx) == 1)
     logs.close_log(log)
 
+def test_match_field_index_with_gaia_catalog():
+
+    params = test_params()
+    log = logs.start_stage_log( params['log_dir'], 'test_crossmatch' )
+
+    xmatch = crossmatch.CrossMatchTable()
+    xmatch.create(params)
+    xmatch = test_field_index(xmatch)
+    xmatch = test_stars_table(xmatch)
+
+    (gaia_data, gaia_star_field_ids) = test_gaia_catalog()
+
+    xmatch.match_field_index_with_gaia_catalog(gaia_data, params, log)
+
+    for g, gaia_field_id in enumerate(gaia_star_field_ids):
+        assert(int(xmatch.field_index['gaia_source_id'][gaia_field_id-1]) == int(gaia_data['source_id'][g]))
+        assert(int(xmatch.stars['gaia_source_id'][gaia_field_id-1]) == int(gaia_data['source_id'][g]))
+        assert(xmatch.stars['gaia_ra'][gaia_field_id-1] == gaia_data['ra'][g])
+        assert(xmatch.stars['gaia_dec'][gaia_field_id-1] == gaia_data['dec'][g])
+
+    logs.close_log(log)
+
 if __name__ == '__main__':
     #test_create()
 #    test_add_dataset()
@@ -240,4 +324,5 @@ if __name__ == '__main__':
     #test_update_field_index()
     #test_assign_quadrants()
     #test_cone_search()
-    test_init_stars_table()
+    #test_init_stars_table()
+    test_match_field_index_with_gaia_catalog()