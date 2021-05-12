b479b44ba77a872bad51f8444d0fbe6b79563fe5
rachel3834 <rstreet@lco.global>
2021-05-12 10:32:46 -0700

    Debugging at runtime

--- trials/assess_diff_images.py
+++ trials/assess_diff_images.py
@@ -13,19 +13,25 @@ def diff_images_qc(params):
 
     dimage_stats = []
     for dimage_path in diff_images:
-        stats = calc_stamp_statistics(dimage_path)
+        stats = calc_stamp_statistics(params,dimage_path)
         dimage_stats.append(stats)
     dimage_stats = np.array(dimage_stats)
 
     plot_dimage_statistics(params, dimage_stats, diff_images)
 
-def calc_stamp_statistics(dimage_path):
+def calc_stamp_statistics(params,dimage_path):
     statistics = []
 
-    stamps = glob.glob(path.join(dimage_path,'diff_stamp_*.fits'))
-    for i,stamp in enumerate(stamps):
+    if params['stamp_number'] == -1:
+        stamps = glob.glob(path.join(dimage_path,'diff_stamp_*.fits'))
+
+        for i,stamp in enumerate(stamps):
+            image = fits.getdata(stamp)
+            statistics.append([i,np.median(image), image.std()])
+    else:
+        stamp = glob.glob(path.join(dimage_path,'diff_stamp_'+str(params['stamp_number'])+'.fits'))
         image = fits.getdata(stamp)
-        statistics.append([i,np.median(image), image.std()])
+        statistics.append([params['stamp_number'],np.median(image), image.std()])
 
     return statistics
 
@@ -33,15 +39,20 @@ def plot_dimage_statistics(params,dimage_stats,diff_images):
 
     markers = ['.', 'v', 's', 'p', '*', '+', 'X', 'd', '1', '3', 'D', '^', 'P', '>', '<', '4']
     col_keys = list(mcolors.TABLEAU_COLORS.keys()) + list(mcolors.TABLEAU_COLORS.keys())
-    print(col_keys, len(col_keys))
 
     dimage_index = np.arange(0,len(diff_images),1)
     frames = []
     for f in diff_images:
         frames.append(path.basename(f))
 
-    (fig, (ax0, ax1)) = plt.subplots(nrows=1, ncols=2)
+    (fig, (ax0, ax1)) = plt.subplots(nrows=2, ncols=1)
     fig.set_size_inches(20, 10)
+
+    if params['stamp_number'] == -1:
+        stamp_index = np.arange(0,dimage_stats.shape[1],1)
+    else:
+        stamp_index = np.array([params['stamp_number']])
+
     for stamp_idx in range(0,dimage_stats.shape[1],1):
         ax0.plot(dimage_index, dimage_stats[:,stamp_idx,1], marker=markers[stamp_idx],
                     markerfacecolor=mcolors.TABLEAU_COLORS[col_keys[stamp_idx]],
@@ -66,7 +77,7 @@ if __name__ == '__main__':
     params = {}
     if len(argv) == 1:
         params['red_dir'] = input('Please enter the path to the reduction directory: ')
-        params['stamp_number'] = int(input('Please enter the stamp number to inspect: '))
+        params['stamp_number'] = int(input('Please enter the stamp number to inspect or -1 for all: '))
     else:
         params['red_dir'] = argv[1]
         params['stamp_number'] = int(argv[2])
