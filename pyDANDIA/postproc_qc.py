a62b48847cb0eebec5d7d4270a9a115efa2d5c4a
rachel3834 <rstreet@lco.global>
2021-08-06 17:01:40 -0700

    Added test statement

--- pyDANDIA/postproc_qc.py
+++ pyDANDIA/postproc_qc.py
@@ -483,11 +483,11 @@ def mask_datapoints_by_image_stamp(photometry, reduction_metadata, bad_data_inde
     stamps = np.unique(bad_data_index[1])
     for s in stamps:
         stamp_dims = reduction_metadata.stamps[1][s]
-        print('Stamp: ',stamp_dims)
-        affected_stars = np.where( (reduction_metadata.star_catalog[1]['x'] >= stamp_dims['X_MIN']) & \
-                        (reduction_metadata.star_catalog[1]['x'] < stamp_dims['X_MAX']) & \
-                        (reduction_metadata.star_catalog[1]['y'] >= stamp_dims['Y_MIN']) & \
-                        (reduction_metadata.star_catalog[1]['y'] < stamp_dims['Y_MAX']) )[0]
+        print('Stamp: ',stamp_dims, reduction_metadata.star_catalog[1]['x'], type(reduction_metadata.star_catalog[1]['x']))
+        affected_stars = np.where( (reduction_metadata.star_catalog[1]['x'] >= stamp_dims['X_MIN'].data) & \
+                        (reduction_metadata.star_catalog[1]['x'] < stamp_dims['X_MAX'].data) & \
+                        (reduction_metadata.star_catalog[1]['y'] >= stamp_dims['Y_MIN'].data) & \
+                        (reduction_metadata.star_catalog[1]['y'] < stamp_dims['Y_MAX'].data) )[0]
 
         #print('AFFECTED STARS: ',reduction_metadata.star_catalog[1][affected_stars])
 
