5102a788a8709fbb83b774c91d635ae6abc077e9
rachel3834 <rstreet@lco.global>
2021-09-13 17:24:34 -0700

    Temp debugging

--- pyDANDIA/lightcurves.py
+++ pyDANDIA/lightcurves.py
@@ -218,6 +218,9 @@ def calc_ps_qc_factor(reduction_metadata,photometry_data,log):
 	iref = np.where(reduction_metadata.headers_summary[1]['IMAGES'] == reference_image_name)
 
 	ps_data = photometry_data['pscale'].data
+	print(ps_data.shape)
+	print(exptimes[iref])
+	print(len(exptimes))
 	mask = np.empty(ps_data.shape)
 	mask.fill(False)
 	invalid = np.where(ps_data == 0.0)
