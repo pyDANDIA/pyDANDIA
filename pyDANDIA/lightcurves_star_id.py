0f3273c9545d2a2591780966fc1f4c4589f0c8ba
rachel3834 <rstreet@lco.global>
2021-09-15 09:31:28 -0700

    Added extraction of filter_name

--- pyDANDIA/lightcurves_star_id.py
+++ pyDANDIA/lightcurves_star_id.py
@@ -25,6 +25,8 @@ def extract_star_lightcurve_star_id(params, log=None, format='dat',
 
     reduction_metadata = metadata.MetaData()
     reduction_metadata.load_all_metadata(params['red_dir'], 'pyDANDIA_metadata.fits')
+	if 'filter_name' not in params:
+		params['filter_name'] = reduction_metadata.headers_summary[1]['FILTKEY'][0]
 
     lc_files = []
 
