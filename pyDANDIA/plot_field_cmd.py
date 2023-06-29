6348e604650f4a15f4f6611c0eefb57c2feaa4fc
rachel3834 <rstreet@lco.global>
2023-06-29 15:36:22 -0700

    Refactored code to generalize it so it can be used with combined datasets

--- pyDANDIA/plot_field_cmd.py
+++ pyDANDIA/plot_field_cmd.py
@@ -13,6 +13,7 @@ from pyDANDIA import plotly_lightcurves
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import argparse
 
 def calc_field_cmds():
 
@@ -25,72 +26,62 @@ def calc_field_cmds():
     xmatch.load(params['crossmatch_file'],log=log)
     log.info('Loaded crossmatch table for the field')
 
-    # List of CMDs to plot.  Each tuple contains the magnitude passband
-    # for the X-axis in the first entry and the two passbands to subtract
-    # to create the colour.
-    # Therefore (g, g, i) -> g vs. (g-i)
-    plot_list = [ ('g', 'g', 'i'), ('g', 'g', 'r'), ('i', 'g', 'i'), ('r', 'r', 'i') ]
-    log.info('Identified list of plots to create')
+    # Select stars with valid photometry in both passbands
+    col1 = params['blue_column']
+    col2 = params['red_column']
+    jdx1 = np.logical_and(xmatch.stars[col1] > 0.0, xmatch.stars[col2] > 0.0)
+    select_stars = np.where(jdx1)[0]
+    log.info(str(len(select_stars))+' stars have valid measurements in both bands')
 
-    # Identify stars with valid measurements.  Since the photometric calibration
-    # is transformed relative to the primary reference, we select the data for
-    # those stars only.
-    primary_ref = 'lsc_doma'
+    # Extract the photometry data for all stars in the field
+    data = np.zeros((len(select_stars),3))
+    data[:,0] = xmatch.stars['field_id'][select_stars]
+    data[:,1] = xmatch.stars[col1][select_stars]
+    data[:,2] = xmatch.stars[col1][select_stars] - xmatch.stars[col2][select_stars]
 
-    # Plot the CMD diagram for each combination of passbands
-    for (f1, f2, f3) in plot_list:
-        col1 = 'cal_'+f1+'_mag_'+primary_ref
-        col2 = 'cal_'+f2+'_mag_'+primary_ref
-        col3 = 'cal_'+f3+'_mag_'+primary_ref
+    # If a specific target was indicated, extract the photometry for that
+    # object
+    if params['target_field_id']:
+        field_idx = params['target_field_id'] - 1
+        target_mag = xmatch.stars[col1][field_idx]
+        target_colour = xmatch.stars[col1][field_idx] - xmatch.stars[col2][field_idx]
+        target_params = [params['target_field_id'], target_mag, target_colour]
+    else:
+        target_params = [None, None, None]
 
-        jdx1 = np.logical_and(xmatch.stars[col1] > 0.0, xmatch.stars[col2] > 0.0)
-        jdx2 = np.logical_and(jdx1, xmatch.stars[col3] > 0.0)
-        select_stars = np.where(jdx2)[0]
-        data = np.zeros((len(select_stars),3))
-        data[:,0] = xmatch.stars['field_id'][select_stars]
-        data[:,1] = xmatch.stars[col1][select_stars]
-        data[:,2] = xmatch.stars[col2][select_stars] - xmatch.stars[col3][select_stars]
-
-        if params['target_field_id']:
-            field_idx = params['target_field_id'] - 1
-            target_mag = xmatch.stars[col1][field_idx]
-            target_colour = xmatch.stars[col2][field_idx] - xmatch.stars[col3][field_idx]
-            target_params = [params['target_field_id'], target_mag, target_colour]
-        else:
-            target_params = [None, None, None]
-
-        log.info(str(len(select_stars))+' stars have valid measurements in '
-                    +f1+', '+f2+' and '+f3+' bands')
-
-        # Plot interactive RMS diagram
-        plot_file = path.join(params['red_dir'], params['plot_file_root']+'_'+f1+'_'+f2+f3+'.html')
-        axis_labels = [f1+' [mag]', '('+f2+'-'+f3+') [mag]']
-        plotly_lightcurves.plot_interactive(data, plot_file, axis_labels,
-                    target_params,
-                    title=params['field_name']+' CMD', xreverse=True)
+    # Plot interactive RMS diagram
+    plot_file = path.join(params['red_dir'], params['plot_file'])
+    axis_labels = [params['xlabel'], params['ylabel']]
+    plotly_lightcurves.plot_interactive(data, plot_file, axis_labels,
+                target_params,
+                title=params['field_name']+' CMD', xreverse=True)
 
     logs.close_log(log)
 
 def get_args():
 
-    params = {}
+    parser = argparse.ArgumentParser(prog='plot_field_cmd')
+    parser.add_argument('red_dir', help='The path to the top-level data directory')
+    parser.add_argument('field_name', help='Name of the field')
+    parser.add_argument('blue_column', help='Xmatch.stars table column name of the bluewards passband')
+    parser.add_argument('red_column', help='Xmatch.stars table column name of the redwards passband')
+    parser.add_argument('xlabel', help='Label for the CMD x-axis')
+    parser.add_argument('ylabel', help='Label for the CMD y-axis')
+    parser.add_argument('target_field_id', help='To highlight a specific object, enter its field ID or None')
+    parser.add_argument('plot_file_name', help='Name of the output plotfile')
 
-    if len(argv) == 1:
+    args = parser.parse_args()
 
-        params['red_dir'] = input('Please enter the path to the top-level data directory: ')
-        params['field_name'] = input('Please enter the name of the field: ')
-        params['target_field_id'] = input('To highlight a specific object, enter its field ID or None: ')
-
-    else:
-
-        params['red_dir'] = argv[1]
-        params['field_name'] = argv[2]
-        if len(argv) == 4:
-            params['target_field_id'] = argv[3]
-
+    params = {'red_dir': args.red_dir,
+              'field_name': args.field_name,
+              'blue_column': args.blue_column,
+              'red_column': args.red_column,
+              'xlabel': args.xlabel,
+              'ylabel': args.ylabel,
+              'target_field_id': args.target_field_id,
+              'plot_file': args.plot_file_name}
     params['crossmatch_file'] = path.join(params['red_dir'],
                             params['field_name']+'_field_crossmatch.fits')
-    params['plot_file_root'] = params['field_name']+'_cmd'
     if 'none' in str(params['target_field_id']).lower():
         params['target_field_id'] = None
     else:
