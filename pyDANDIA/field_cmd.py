71c0940507da4ee6ad82eb9d4a8702c847c4d0a1
rachel3834 <rstreet@lco.global>
2023-11-01 13:51:25 -0700

    Updating phot quality control cuts

--- pyDANDIA/field_cmd.py
+++ pyDANDIA/field_cmd.py
@@ -334,12 +334,12 @@ def select_by_photometry_quality(xmatch, config, log):
     icol = 'cal_i_mag_'+config['reference_dataset_code']
     ierrcol = 'cal_i_magerr_'+config['reference_dataset_code']
 
-    qc_idx1 = np.where(np.logical_and(np.greater(xmatch.stars[gcol], 0.0),
-                                      np.less_equal(xmatch.stars[gerrcol], config['g_sigma_max'])))[0]
-    qc_idx2 = np.where(np.logical_and(np.greater(xmatch.stars[rcol], 0.0),
-                                      np.less_equal(xmatch.stars[rerrcol], config['r_sigma_max'])))[0]
-    qc_idx3 = np.where(np.logical_and(np.greater(xmatch.stars[icol], 0.0),
-                                      np.less_equal(xmatch.stars[ierrcol], config['i_sigma_max'])))[0]
+    qc_idx1 = np.where(np.logical_and(xmatch.stars[gcol] > 0.0,
+                                      xmatch.stars[gerrcol] <= config['g_sigma_max']))[0]
+    qc_idx2 = np.where(np.logical_and(xmatch.stars[rcol] > 0.0,
+                                      xmatch.stars[rerrcol] <= config['r_sigma_max']))[0]
+    qc_idx3 = np.where(np.logical_and(xmatch.stars[icol] >  0.0,
+                                      xmatch.stars[ierrcol] <= config['i_sigma_max']))[0]
 
     # qc_idx1 = np.where( np.logical_and( np.less_equal(xmatch.stars['cal_g_magerr_'+config['reference_dataset_code']], config['g_sigma_max']),
     #                     np.less_equal(xmatch.stars['cal_r_magerr_'+config['reference_dataset_code']], config['r_sigma_max']) ) )[0]
@@ -349,7 +349,7 @@ def select_by_photometry_quality(xmatch, config, log):
     #                     np.less_equal(xmatch.stars['(g-r)_error'], config['gr_sigma_max']) ) )[0]
     qc_idx = set(qc_idx1).intersection(set(qc_idx2))
     qc_idx = np.array(list(qc_idx.intersection(set(qc_idx3))))
-    
+
     log.info(' -> '+str(len(qc_idx))+' stars meet the quality selection criteria:')
     log.info('    Max phot uncertainty, g = '+str(config['g_sigma_max']))
     log.info('    Max phot uncertainty, r = '+str(config['r_sigma_max']))
