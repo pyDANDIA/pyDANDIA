c3cffdde9a80d4613d96850e62db53a47065853e
rachel3834 <rstreet@lco.global>
2024-10-24 11:50:21 -0700

    Correcting json formatting

--- pyDANDIA/field_cmd.py
+++ pyDANDIA/field_cmd.py
@@ -28,7 +28,7 @@ def field_colour_analysis():
     for xdata, ydata in config['cmd_plot_list'].items():
         plot_field_colour_mag_diagram(config, xmatch, valid_stars, selected_stars, xdata, ydata, log)
 
-    if config['ccd_plot']:
+    if 'true' in str(config['ccd_plot']).lower():
         plot_field_colour_colour_diagram(config, xmatch, valid_stars, selected_stars, log)
 
     output_photometry(config, xmatch, selected_stars, log)
