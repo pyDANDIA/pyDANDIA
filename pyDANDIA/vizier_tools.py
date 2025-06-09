858904be5b022445ef054b991090335e5e0e0f92
rachel3834 <rstreet@lco.global>
2025-06-09 10:12:31 -0700

    Switched off vizier sserver selection

--- pyDANDIA/vizier_tools.py
+++ pyDANDIA/vizier_tools.py
@@ -76,8 +76,8 @@ def search_vizier_for_sources(ra, dec, radius, catalog, row_limit=-1,
 
     # Default to simple Vizier query since multi-server approach producing
     # max-retry errors
-    #result = v.query_region(c, radius=r, catalog=cat_id)
-    (status, result) = query_vizier_servers(v, c, r, [cat_id], debug=debug)
+    result = v.query_region(c, radius=r, catalog=cat_id)
+    #(status, result) = query_vizier_servers(v, c, r, [cat_id], debug=debug)
 
     if result != None and len(result) == 1:
 
@@ -103,9 +103,10 @@ def query_vizier_servers(query_service, coord, search_radius, catalog_id, log=No
     catalog_id  str      Name of catalog to be searched in ViZier's notation
     """
 
-    #vizier_servers_list = ['vizier.cfa.harvard.edu', 'vizier.hia.nrc.ca', 'vizier.u-strasbg.fr']
+    # Apparently 'vizier.cfa.harvard.edu' is unavailable
+    vizier_servers_list = ['vizier.hia.nrc.ca', 'vizier.u-strasbg.fr']
 
-    #query_service.VIZIER_SERVER = vizier_servers_list[0]
+    query_service.VIZIER_SERVER = vizier_servers_list[0]
 
     query_service.TIMEOUT = 60
 
